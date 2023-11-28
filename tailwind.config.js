eb5075584d81ffd12bb8bbde0a2648e6b7f0428e
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-11-28 09:27:17 +0700

    update home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'lato':'Lato',
+        'poppins':'Poppins',
+        'openSans':'Open Sans',
+        'novaSquare':'Nova Square'
+      }
+    },
   },
   plugins: [],
 }
-

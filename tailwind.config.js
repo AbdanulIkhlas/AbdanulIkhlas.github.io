cfc7710ce4be004740dd60da3cc33d77acbf1f29
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-11-29 16:06:42 +0700

    menambahkan custom extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
         'poppins':'Poppins',
         'openSans':'Open Sans',
         'novaSquare':'Nova Square'
+      },
+      colors: {
+        'primaryColor': '#950101',
+        'secondaryColor':'#03002C',
+        'customYellow':'#F0C929',
+        'bgSocial':'#f1e509d4'
+      },
+      boxShadow: {
+        'socialShadow':'0 0 20px #fffb01'
       }
     },
   },
